d9385cb664a45eb2dc303ee207b6938761fd8fd3
lunosat <akirah.ofc@gmail.com>
2024-05-20 17:13:13 -0300

    fc

---
+++ app.js
@@ -0,0 +1,48 @@
+import express from 'express';
+import axios from 'axios';
+import cors from 'cors';
+import cheerio from 'cheerio';
+import fs from 'fs';
+import https from 'https';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.get('/profile-image', async (req, res) => {
+    const { phone } = req.query;
+
+    if (!phone) {
+        return res.status(400).json({ error: 'Phone number is required' });
+    }
+
+    try {
+        const whatsappURL = `https://api.whatsapp.com/send/?phone=${phone}`;
+        const response = await axios.get(whatsappURL);
+        
+        const $ = cheerio.load(response.data);
+        const imageElement = $('img[alt="Profile Picture"]');
+
+        if (imageElement.length > 0) {
+            const imageUrl = imageElement.attr('src');
+            return res.json({ imageUrl });
+        } else {
+            return res.status(404).json({ error: 'Profile image not found' });
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'Failed to fetch profile image' });
+    }
+});
+
+const privateKey = fs.readFileSync('key.pem', 'utf8');
+const certificate = fs.readFileSync('cert.pem', 'utf8');
+
+const credentials = { key: privateKey, cert: certificate };
+
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(port, () => {
+    console.log(`HTTPS Server running at https://localhost:${port}`);
+});
